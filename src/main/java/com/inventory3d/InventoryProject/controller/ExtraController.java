c2d5121e1ba68093cb2fe03be44328496c72dd66
StelianVana <vanastelian@gmail.com>
2024-03-28 00:21:14 +0100

    3D Printing Inventory System

--- src/main/java/com/inventory3d/InventoryProject/controller/ExtraController.java
+++ src/main/java/com/inventory3d/InventoryProject/controller/ExtraController.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.Optional;
 
